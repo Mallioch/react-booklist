97139265849286ac150828dd825faea582852afe
Eric Sowell <eric.sowell@gmail.com>
2017-05-23 11:05:19 -0500

    changed to native promises

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27020/test');
 
 // Priority serve any static files.
